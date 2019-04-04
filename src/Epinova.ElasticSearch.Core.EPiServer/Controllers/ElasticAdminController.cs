d08b080ea8225f4c569bf338225f66ae2d07526f
Robin Holzner <robin.holzner@epinova.no>
2019-04-04 22:54:37 +0200

    TASK63: ElasticAdminController has no build error

--- src/Epinova.ElasticSearch.Core.EPiServer/Controllers/ElasticAdminController.cs
+++ src/Epinova.ElasticSearch.Core.EPiServer/Controllers/ElasticAdminController.cs
@@ -2,151 +2,77 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
-using Epinova.ElasticSearch.Core.Admin;
-using Epinova.ElasticSearch.Core.Contracts;
 using Epinova.ElasticSearch.Core.EPiServer.Controllers.Abstractions;
-using Epinova.ElasticSearch.Core.EPiServer.Models.ViewModels;
-using Epinova.ElasticSearch.Core.Models;
-using Epinova.ElasticSearch.Core.Models.Admin;
-using Epinova.ElasticSearch.Core.Settings;
-using Epinova.ElasticSearch.Core.Settings.Configuration;
-using Epinova.ElasticSearch.Core.Utilities;
 using EPiServer.DataAbstraction;
+using Microsoft.Azure.Search;
 
 namespace Epinova.ElasticSearch.Core.EPiServer.Controllers
 {
     public class ElasticAdminController : ElasticSearchControllerBase
     {
         private readonly ILanguageBranchRepository _languageBranchRepository;
-        private readonly ICoreIndexer _coreIndexer;
-        private readonly IElasticSearchSettings _settings;
-        private static Health _healthHelper;
+        private static SearchServiceClient _serviceClient;
 
-        public ElasticAdminController(
-            ILanguageBranchRepository languageBranchRepository,
-            ICoreIndexer coreIndexer,
-            IElasticSearchSettings settings)
+
+        public ElasticAdminController(ILanguageBranchRepository languageBranchRepository)
         {
             _languageBranchRepository = languageBranchRepository;
-            _coreIndexer = coreIndexer;
-            _settings = settings;
-            _healthHelper = new Health(settings);
+            _serviceClient = CreateSearchServiceClient();
         }
 
         [Authorize(Roles = "ElasticsearchAdmins")]
         public ActionResult Index()
         {
-            HealthInformation clusterHealth = _healthHelper.GetClusterHealth();
-            Node[] nodeInfo = _healthHelper.GetNodeInfo();
-
-            var indexHelper = new Index(_settings);
-
-            var allIndices = indexHelper.GetIndices();
-
-            ElasticSearchSection config = ElasticSearchSection.GetConfiguration();
-
-            foreach (var index in allIndices)
-            {
-                var parsed = config.IndicesParsed.FirstOrDefault(i =>
-                    index.Index.StartsWith(i.Name, StringComparison.InvariantCultureIgnoreCase));
-
-                index.Type = String.IsNullOrWhiteSpace(parsed.Type)
-                    ? "[default]"
-                    : Type.GetType(parsed.Type).Name;
-            }
-
-            var adminViewModel = new AdminViewModel(clusterHealth, allIndices.OrderBy(i => i.Type), nodeInfo);
-
-            return View("~/Views/ElasticSearchAdmin/Admin/Index.cshtml", adminViewModel);
+            return View("~/Views/ElasticSearchAdmin/Admin/Index.cshtml", _serviceClient.Indexes.List().Indexes);
         }
 
-        [Authorize(Roles = "ElasticsearchAdmins")]
-        public ActionResult AddNewIndex()
+        private IEnumerable<string> GetIndexNames()
         {
-            if (Core.Server.Info.Version.Major < 5)
-                throw new Exception("Elasticsearch version 5 or higher required");
-
-            ElasticSearchSection config = ElasticSearchSection.GetConfiguration();
+            //ElasticSearchSection config = ElasticSearchSection.GetConfiguration();
 
             IEnumerable<string> languages = _languageBranchRepository
                 .ListEnabled()
                 .Select(lang => lang.LanguageID);
 
+            var result = new List<string>();
+
             foreach (string lang in languages)
             {
-                foreach (IndexConfiguration indexConfig in config.IndicesParsed)
-                {
-                    var indexName = _settings.GetCustomIndexName(indexConfig.Name, lang);
-                    Type indexType = GetIndexType(indexConfig, config);
-
-                    var index = new Index(_settings, indexName);
-
-                    if (!index.Exists)
-                    {
-                        index.Initialize(indexType);
-                        index.WaitForStatus();
-                    }
-
-                    if (IsCustomType(indexType))
-                    {
-                        index.DisableDynamicMapping(indexType);
-                        _coreIndexer.UpdateMapping(indexType, indexType, indexName, lang, true);
-                        index.WaitForStatus();
-                    }
-                    else if(_settings.CommerceEnabled)
-                    {
-                        indexName = _settings.GetCustomIndexName($"{indexConfig.Name}-{Constants.CommerceProviderName}", lang);
-                        index = new Index(_settings, indexName);
-                        if (!index.Exists)
-                        {
-                            index.Initialize(indexType);
-                            index.WaitForStatus();
-                        }
-                    }
-                }
+                var indexName = CreateIndexName("test", lang);
+                result.Add(indexName);
             }
-
-            return RedirectToAction("Index");
+            return result;
         }
 
-        private static bool IsCustomType(Type indexType)
+        private static string CreateIndexName(string index, string language)
         {
-            return indexType != null && indexType != typeof(IndexItem);
-        }
+            if (String.IsNullOrWhiteSpace(index))
+                throw new InvalidOperationException("Index must be specified");
+            if (String.IsNullOrWhiteSpace(language))
+                throw new InvalidOperationException("Language must be specified");
 
-        private static Type GetIndexType(IndexConfiguration index, ElasticSearchSection config)
-        {
-            if(index.Default || config.IndicesParsed.Length == 1)
-                return typeof(IndexItem);
-
-            if (String.IsNullOrWhiteSpace(index.Type))
-                return null;
-
-            return Type.GetType(index.Type, false, true);
+            return $"{index}-{language}".ToLower();
         }
 
         [Authorize(Roles = "ElasticsearchAdmins")]
-        public ActionResult DeleteIndex(string indexName)
+        public ActionResult AddNewIndex()
         {
-            var indexing = new Indexing(_settings);
-            indexing.DeleteIndex(indexName);
+            //if (Core.Server.Info.Version.Major < 5)
+            //    throw new Exception("Elasticsearch version 5 or higher required");
+
+
 
             return RedirectToAction("Index");
         }
 
-        [Authorize(Roles = "ElasticsearchAdmins")]
-        public ActionResult ChangeTokenizer(string indexName, string tokenizer)
+        private SearchServiceClient CreateSearchServiceClient()
         {
-            var indexing = new Indexing(_settings);
-            var index = new Index(_settings, indexName);
+            //TODO: change to use not hardcoded value
+            string searchServiceName = "SearchServiceName";
+            string adminApiKey = "SearchServiceAdminApiKey";
 
-            indexing.Close(indexName);
-            index.ChangeTokenizer(tokenizer);
-            indexing.Open(indexName);
-
-            index.WaitForStatus();
-
-            return RedirectToAction("Index");
+            SearchServiceClient serviceClient = new SearchServiceClient(searchServiceName, new SearchCredentials(adminApiKey));
+            return serviceClient;
         }
     }
 }
--- src/Epinova.ElasticSearch.Core.EPiServer/Models/ViewModels/AdminViewModel.cs
+++ src/Epinova.ElasticSearch.Core.EPiServer/Models/ViewModels/AdminViewModel.cs
@@ -1,19 +1,14 @@
 ﻿using System.Collections.Generic;
-using Epinova.ElasticSearch.Core.Models.Admin;
 
 namespace Epinova.ElasticSearch.Core.EPiServer.Models.ViewModels
 {
     public class AdminViewModel
     {
-        public AdminViewModel(HealthInformation clusterHealth, IEnumerable<IndexInformation> allIndexes, Node[] nodeInfo)
+        public AdminViewModel(IEnumerable<Microsoft.Azure.Search.Models.Index> allIndexes)
         {
-            NodeInfo = nodeInfo;
-            ClusterHealth = clusterHealth;
             AllIndexes = allIndexes;
         }
 
-        public Node[] NodeInfo { get; }
-        public HealthInformation ClusterHealth { get; }
-        public IEnumerable<IndexInformation> AllIndexes { get; }
+        public IEnumerable<Microsoft.Azure.Search.Models.Index> AllIndexes { get; }
     }
 }
